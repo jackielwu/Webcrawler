2aeac9a0a83cbbfe2c076eebf419dd4bfe7c3d79
Jackie Wu <jackielwu@gmail.com>
2017-06-26 13:58:44 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -2,3 +2,21 @@
 #include "webcrawler.h"
 
 // Add your implementation here
+WebCrawler::WebCrawler(int maxUrls, int nurlRoots, const char ** urlRoots)
+{
+
+}
+
+void WebCrawler::crawl()
+{
+
+}
+
+void WebCrawler::writeURLFile(const char * urlFileName)
+{
+
+}
+void WebCrawler::writeWordFile(const char *wordFilename)
+{
+
+}
