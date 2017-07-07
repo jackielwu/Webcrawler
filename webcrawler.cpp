1840ec7cbb232f0f6ae1c470f371a09d862a48bc
Jackie Wu <jackielwu@gmail.com>
2017-07-07 16:28:26 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -14,6 +14,7 @@ WebCrawler::WebCrawler(int maxUrls, int nurlRoots, const char ** urlRoots)
 	{
 	  URLRecord *e = new URLRecord;
 	  e->_url = (char *) urlRoots[i];
+	  e->_i = 0;
 	  _urlArray[_tailURL++] = *e;
 	}
 }
