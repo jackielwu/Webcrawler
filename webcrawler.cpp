cc7792076754064bb1cf4a65110c2076c006c146
Jackie Wu <jackielwu@gmail.com>
2017-07-07 16:06:30 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -108,7 +108,7 @@ void WebCrawler::onAnchorFound(char * url)
   			_urlToUrlRecord.insert(url, _tailURL);
   		}
   		else if(u.find("http") == string::npos) {
-  			u.insert(0, '/');//handle separation for relative links
+  			u.insert(0, "/");//handle separation for relative links
   			u.insert(0, _urlArray[_headURL]._url);
   			_urlArray[_tailURL++]._url = (char *)u.c_str();
   		}
