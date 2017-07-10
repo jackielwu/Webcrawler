173280832dac750703545976a4d3c5f45a152d76
Jackie Wu <jackielwu@gmail.com>
2017-07-10 15:04:57 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -179,7 +179,7 @@ int main( int argc, char ** argv )
   
   if ( !strcmp(option,"") ) {
 	  // default 1000 maxURLs
-	  maxURL = 100;
+	  maxURL = 10;
 	  nURL = 1;
 	  WebCrawler wc(maxURL, nURL, (const char **)url);
 	  wc.crawl();
